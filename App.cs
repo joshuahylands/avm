a8bcd496bf7cd352859d06abfd73d4dbee00ed48
Joshua Hylands <hylands.joshua2002@gmail.com>
2023-09-07 21:41:04 +0100

    Moved the tray icon out of the main window and is the only thing that is present when the app launches

--- App.cs
+++ App.cs
@@ -1,5 +1,7 @@
 using AVM.Views;
+using H.NotifyIcon;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
 
 namespace AVM;
 
@@ -7,13 +9,31 @@ partial class App : Application
 {
   public readonly static MainWindow MainWindow = new();
 
+  private TaskbarIcon? _trayIcon;
+
   public App()
   {
     InitializeComponent();
   }
 
+  // Setup the tray icon when the app launches
   protected override void OnLaunched(LaunchActivatedEventArgs e)
   {
-    MainWindow.Activate();
+    ((XamlUICommand) Resources["ShowMainWindow"]).ExecuteRequested += ShowMainWindow;
+    ((XamlUICommand) Resources["ExitApplication"]).ExecuteRequested += ExitApplication;
+
+    _trayIcon = (TaskbarIcon) Resources["TrayIcon"];
+    _trayIcon.ForceCreate();
+  }
+
+  private void ShowMainWindow(object sender, ExecuteRequestedEventArgs args) => MainWindow.Activate();
+
+  private void ExitApplication(object sender, ExecuteRequestedEventArgs args)
+  {
+    MainWindow.Close();
+
+    _trayIcon?.Dispose();
+
+    Exit();
   }
 }
